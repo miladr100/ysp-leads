0d58ab8a847b0aff00e033352cb06dc51ec72cef
Milad Roghanian <miladr100@gmail.com>
2021-08-18 07:57:20 -0400

    feat: creat datatable page

---
+++ pages/datatable.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-row>
+        <data-table></data-table>
+    </v-row>
+</template>
+
+<script>
+import DataTableComponent from "vue-materialize-datatable";
+
+export default {
+  name: 'Datatable',
+  components: {
+      "data-table": DataTableComponent,
+  },
+  data() {
+    return { }
+  }
+}
+</script>
